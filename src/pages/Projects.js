6c9dc0e01ae7d6137812fb75bed46c1a34fed43d
RaeesHaider321 <raees.haider@bodlabuilders.com.pk>
2025-04-17 17:08:38 +0500

    map upload

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -190,16 +190,7 @@ const Projects = () => {
               <h2>Locate our Projects on Map</h2>
               <Row>
                 <Col md={12}>
-                  <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3444.9287473577783!2d71.55361441037263!3d30.296090006500197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3924cbd1167128b5%3A0x79d6c7b73dd41cef!2sBusiness%20Hub%2C%20DHA%20Multan!5e0!3m2!1sen!2s!4v1744012574158!5m2!1sen!2s"
-                    width="100%"
-                    height="350px"
-                    style={{ border: 0 }}
-                    allowFullScreen
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"
-                    title="Google Maps location of Bodla Bulders, Bodla Group Business Hub, DHA Multan"
-                  />
+                <iframe src="https://www.google.com/maps/d/u/2/embed?mid=1YNuh7UDSkSH5G9rC3CXM52j_uytYDgQ&ehbc=2E312F&noprof=1" width="100%" height="380"></iframe>
                 </Col>
               </Row>
             </CardBody>
