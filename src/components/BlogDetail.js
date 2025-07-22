9aa18bd6123d521b9fc163ebb77a719a04879ea9
RaeesHaider321 <raees.haider@bodlabuilders.com.pk>
2025-07-22 13:29:38 +0500

    blog

--- src/components/BlogDetail.js
+++ src/components/BlogDetail.js
@@ -1,15 +1,34 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { blogPosts } from "../data/blogData";
-import { Container, Row, Col, Button, Image, Table, Accordion, Card } from 'react-bootstrap';
+import { Container, Row, Col, Button, Image, Table, Accordion, Card, ButtonGroup } from 'react-bootstrap';
+import { Facebook, Twitter, Linkedin, Link as LinkIcon, Whatsapp } from 'react-bootstrap-icons';
 import "../styles/blogs.css";
 
 const BlogDetail = () => {
-  const { slug } = useParams(); // Changed from id to slug
+  const { slug } = useParams();
   const navigate = useNavigate();
-  const blog = blogPosts.find(post => post.slug === slug); // Changed lookup
-
-
+  const blog = blogPosts.find(post => post.slug === slug);
+  
   if (!blog) return <p>Blog not found.</p>;
+
+  // Get current URL for sharing
+  const currentUrl = window.location.href;
+  const shareTitle = encodeURIComponent(blog.title);
+  const shareText = encodeURIComponent(`Check out this blog post: ${blog.title}`);
+
+  // Social share URLs
+  const socialLinks = {
+    facebook: `https://www.facebook.com/sharer/sharer.php?u=${currentUrl}`,
+    twitter: `https://twitter.com/intent/tweet?url=${currentUrl}&text=${shareText}`,
+    linkedin: `https://www.linkedin.com/shareArticle?mini=true&url=${currentUrl}&title=${shareTitle}`,
+    whatsapp: `https://wa.me/?text=${shareText}%20${currentUrl}`
+  };
+
+  // Copy URL to clipboard
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(currentUrl);
+    alert('Link copied to clipboard!');
+  };
 
   return (
     <section className="py-4">
@@ -31,6 +50,48 @@ const BlogDetail = () => {
               <small>By {blog.author} on {blog.date}</small>
             </p>
 
+            {/* Social Share Buttons */}
+            <div className="social-share mb-4">
+              <h5>Share this post:</h5>
+              <ButtonGroup aria-label="Social share buttons">
+                <Button 
+                  variant="outline-primary" 
+                  onClick={() => window.open(socialLinks.facebook, '_blank')}
+                  aria-label="Share on Facebook"
+                >
+                  <Facebook /> Facebook
+                </Button>
+                <Button 
+                  variant="outline-info" 
+                  onClick={() => window.open(socialLinks.twitter, '_blank')}
+                  aria-label="Share on Twitter"
+                >
+                  <Twitter /> Twitter
+                </Button>
+                <Button 
+                  variant="outline-primary" 
+                  onClick={() => window.open(socialLinks.linkedin, '_blank')}
+                  aria-label="Share on LinkedIn"
+                >
+                  <Linkedin /> LinkedIn
+                </Button>
+                <Button 
+                  variant="outline-success" 
+                  onClick={() => window.open(socialLinks.whatsapp, '_blank')}
+                  aria-label="Share on WhatsApp"
+                >
+                  <Whatsapp /> WhatsApp
+                </Button>
+                <Button 
+                  variant="outline-secondary" 
+                  onClick={copyToClipboard}
+                  aria-label="Copy link"
+                >
+                  <LinkIcon /> Copy Link
+                </Button>
+              </ButtonGroup>
+            </div>
+
             {blog.contentSections?.map((section, index) => (
               <div id={section.id} key={index} className="scroll-section mb-4">
                 <p>{section.intro}</p>
@@ -42,7 +103,6 @@ const BlogDetail = () => {
               </div>
             ))}
 
-            
             {blog.faq && blog.faq.length > 0 && (
               <>
                 <h4 className="mt-5">Frequently Asked Questions (FAQs)</h4>
@@ -56,9 +116,9 @@ const BlogDetail = () => {
                 </Accordion>
               </>
             )}
-
           </Col>
-          <Col xs={12} md={5} lg={4} className="sticky-top" >
+          
+          <Col xs={12} md={5} lg={4} className="sticky-top">
             <Card className="tableOfContent">
               <Card.Body>
                 {blog.listItems && blog.listItems.length > 0 && (
@@ -67,10 +127,9 @@ const BlogDetail = () => {
                     <ul>
                       {blog.listItems.map((item, index) => (
                         <li key={index}>
-                          {/* <a href={`#${item.toLowerCase().replace(/\s+/g, '-').replace(/[^\w-]/g, '')}`} className="toc-link">
+                          <a href={`#${blog.contentSections[index].id}`} className="toc-link">
                             {item}
-                          </a> */}
-                          <a href={`#${blog.contentSections[index].id}`} className="toc-link">{item}</a>
+                          </a>
                         </li>
                       ))}
                     </ul>
@@ -78,7 +137,6 @@ const BlogDetail = () => {
                 )}
               </Card.Body>
             </Card>
-
           </Col>
         </Row>
       </Container>
